ffda04b5ee55f9105c386cf014107bb2de91ec9b
Tom Harris <tomharris88@outlook.com>
2022-04-20 20:23:57 -0500

    save and load data from file system

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const fs = require("fs");
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -10,12 +11,31 @@ const createWindow = () => {
         }
     });
 
+    ipcMain.on('save-data', (event, apps) => {
+        try {
+            fs.writeFileSync("c:\\temp\\test.log", apps);
+        } catch (error){
+            console.error(error);
+        }
+    });
+
     win.loadFile("app/index.html");
 };
 
 app.whenReady().then(() => {
-    createWindow();
+    process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+    ipcMain.handle("loadData", () => {
+        try {
+            const fileText = fs.readFileSync("c:\\temp\\test.log", "utf-8");
+            const jsonObject = JSON.parse(fileText);
+            return jsonObject;
+        } catch (error){
+            console.error(error);
+        }
+    });
 
+    createWindow();
+    
     app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
--- src/preload.js
+++ src/preload.js
@@ -1,3 +1,10 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+    saveData: (apps) => ipcRenderer.send("save-data", apps),
+    loadData: () => ipcRenderer.invoke("loadData")
+});
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
         const element = document.getElementById(selector)
